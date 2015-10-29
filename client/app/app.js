f70c2dc269f0e25e85f3a7c542417144c8d8bc11
Kevin Silas <Kwsilas@gmail.com>
2015-10-29 12:34:55 -0400

    add chart to stores controller

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('truthInGivingApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'tc.chartjs'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
